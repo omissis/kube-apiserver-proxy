94aa9b2a066e0e6cfdf9fa3fa3901c9014530557
omissis <claudi0.beatric3@gmail.com>
2023-04-29 18:52:58 +0200

    feat: introduce openapi to graphql conversion

--- internal/graph/resolver.go
+++ internal/graph/resolver.go
@@ -1,11 +1,7 @@
 package graph
 
-import "github.com/omissis/kube-apiserver-proxy/internal/graph/model"
-
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct {
-	TodoStore []*model.Todo
-}
+type Resolver struct{}
