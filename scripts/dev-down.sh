b9b8fa0b0302c52e30c6a157328b915e87e5044e
omissis <claudi0.beatric3@gmail.com>
2023-05-01 00:51:50 +0200

    feat: make registry stop when running dev-down without force flag

--- scripts/dev-down.sh
+++ scripts/dev-down.sh
@@ -9,6 +9,9 @@ docker rm -f "${PROJECT_NAME}-kind-control-plane"
 if [[ "${FORCE}" -eq "1" ]]; then
   echo "Deleting local registry..."
   docker rm -f "${PROJECT_NAME}-kind-registry"
+else
+  echo "Stopping local registry..."
+  docker stop "${PROJECT_NAME}-kind-registry"
 fi
 
 echo "Done."
