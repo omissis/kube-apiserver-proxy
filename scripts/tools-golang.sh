be3365de65d30e1c7ee9de6602358c5765b64b52
Claudio Beatrice <claudi0.beatric3@gmail.com>
2023-07-06 10:59:27 +0200

    chore: add gomock dep, remove old script

--- scripts/tools-golang.sh
+++ scripts/tools-golang.sh
@@ -8,5 +8,6 @@ go install github.com/davidrjenni/reftools/cmd/fillstruct@latest
 go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
 go install github.com/nikolaydubina/go-cover-treemap@latest
 go install github.com/onsi/ginkgo/v2/ginkgo@latest
+go install go.uber.org/mock/mockgen@latest
 go install golang.org/x/tools/cmd/goimports@latest
 go install mvdan.cc/gofumpt@latest
